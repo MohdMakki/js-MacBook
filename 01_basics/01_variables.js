7215889c74f07940ba2573e0e36bf193548a7f09
MOHD MAKKI <92999426+MohdMakki@users.noreply.github.com>
2023-12-28 04:01:52 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "mohdmakki27@gmail.com"
+var accountPassward = "1234567"
+
+let accountState
+
+accountCity = "Lucknow"
+
+// accountId = 2
+
+accountEmail = "hc@gmail.com"
+accountPassward = "1213131"
+accountCity = "Bangluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassward, accountCity, accountState])
